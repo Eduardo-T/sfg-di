1b934418cab235f8e0435af5f4be997d02eb3c60
Eduardo-T <laloxgames@gmail.com>
2022-05-04 10:44:30 -0500

    Initial Commit

---
+++ src/test/java/me/eduardo/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eduardo.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
